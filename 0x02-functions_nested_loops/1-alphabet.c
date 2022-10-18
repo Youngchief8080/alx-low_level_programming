3aaf7c464e8ab80deb1747244f30ad530af87ac3
Youngchief8080 <chatwithakpe@gmail.com>
2022-10-18 13:52:27 -0700

    print all alphabelt in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print all alphabelt in lowercase
+ */
+void alphabelt(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+
+		_putchar(letter);
+
+	_putchar('\n');
+}
